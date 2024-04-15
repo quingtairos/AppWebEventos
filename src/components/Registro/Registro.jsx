fc288ad9d2d7b8649972259d00a12799a70fb658
maria <mariaquintairosfernandez03@liceolapaz.net>
2024-04-15 11:58:24 +0200

    crear carpeta registro con sus archivos correspondientes y crear la configuración de los archivos correpondientes

---
+++ src/components/Registro/Registro.jsx
@@ -0,0 +1,11 @@
+import './Registro.css';
+
+const Registro = () => {
+    /* return (
+        <div className="registro">
+            <h1>Registro</h1>
+        </div>
+    ) */
+}
+
+export default Registro;
