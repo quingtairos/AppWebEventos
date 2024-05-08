a68f6d56e1dad4db3a2d40a5149455b9fc79d34c
maria <mariaquintairosfernandez03@liceolapaz.net>
2024-05-09 01:23:49 +0200

    modificar el archivo header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -19,13 +19,25 @@ import { NavLink } from 'react-router-dom';
                         <img src="../../assets/img/user-circle.png" alt="" />
                         <ul>
                             <li><NavLink to={"../Login/Login.jsx"}>Iniciar Sesión</NavLink></li>
-                            <li><NavLink to={"../Registro/Registro.jsx"}></NavLink></li>
+                            <li><NavLink to={"../Registro/Registro.jsx"}>Registrate</NavLink></li>
                         </ul>
                         <li><NavLink to={"../GestionEventosPagina/GestionEventosPagina.jsx"}></NavLink></li>
                     </ul>
                 </nav>
             </header>
         )
+        {
+            usuario && /*(
+                <li>
+                    <NavLink to={"/perfil"}>{usuario.nombre}</NavLink>
+                    <NavLink to={"/logout"}>Cerrar Sesión</NavLink>
+                </li>
+            )*/
+            <div className='profile'>
+                <h1>{usuario.displayName}</h1>
+                <img src="{user.photoURL}" alt="user" />
+            </div>
+        }
     }
 
 export default Header;
