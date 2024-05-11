86ff9f746d9fc6de17f74cd1f8ddb66ca1b833f2
maria <mariaquintairosfernandez03@liceolapaz.net>
2024-05-11 14:33:14 +0200

    añadir configuraciones al archivo de producto

--- src/components/Producto/Producto.jsx
+++ src/components/Producto/Producto.jsx
@@ -1,6 +1,23 @@
 import './Producto.css'; 
 
     const Producto = () => {
+
+        const productosPorPag = 5;
+
+        return (
+            <div className="producto">
+                {/* <h1>Producto</h1> */}
+                <div className='busqueda'>
+                    <input placeholder='Buscar producto'/>
+                </div>
+                <button onClick={() => detalleProducto()}>Ver detalle</button>
+                <DetalleProducto />
+            </div>
+        )
+
+        /*  const detalleProducto = () => {
+            return <DetalleProducto />
+        } */
         /* return (
             <div className="producto">
                 <h1>Producto</h1>
