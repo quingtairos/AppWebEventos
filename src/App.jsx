c6ab510e3d88d54d29701c8dafb9833a2bc709db
maria <mariaquintairosfernandez03@liceolapaz.net>
2024-05-12 13:21:47 +0200

    añadir import de la imagen

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import Footer from './components/Footer/Footer';
 import Producto from './components/Producto/Producto';
 
 //import user-circle from './images/user-circle.png';
-//import fest from './assets/img/fest.jpeg';
+import fest from './assets/img/fest.jpeg';
 
 //import { useState } from 'react';
 
