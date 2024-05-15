e550be54ce9509afc4d309208a19973072af279a
maria <mariaquintairosfernandez03@liceolapaz.net>
2024-05-15 17:28:51 +0200

    hacer modificaciones y solucionar error

--- src/components/GestionEventosPagina/GestionEventosPagina.jsx
+++ src/components/GestionEventosPagina/GestionEventosPagina.jsx
@@ -30,6 +30,17 @@ import * as React from 'react';
                 <p>
                 Aquí encontrarás todo lo que necesitas para organizar tus eventos especiales.
                 ¡Descubre nuestra selección de productos y haz que tus celebraciones sean inolvidables!
+                {/* <model-viewer src="Upload model..." ar ar-modes="webxr scene-viewer quick-look" camera-controls tone-mapping="neutral" poster="poster.webp" shadow-intensity="1">
+                    <div class="progress-bar hide" slot="progress-bar">
+                        <div class="update-bar"></div>
+                    </div>
+                    <button slot="ar-button" id="ar-button">
+                        View in your space
+                    </button>
+                    <div id="ar-prompt">
+                        <img src="https://modelviewer.dev/shared-assets/icons/hand.png" />
+                    </div>
+                </model-viewer> */}
                 </p>
                  {/*  más contenido, como mapa, enlaces, ... */}
                  
