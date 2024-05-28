eea66eeea9df78f316d091c20f1c11fb01254e32
maria <mariaquintairosfernandez03@liceolapaz.net>
2024-05-29 01:56:16 +0200

    añadir configuracion a la àf de productos

--- src/components/Producto/Producto.jsx
+++ src/components/Producto/Producto.jsx
@@ -1,10 +1,21 @@
 import './Producto.css';
 
+import * as React from 'react';
+
 import DetalleProducto from '../DetalleProducto';
 
-    const Producto = () => {
+    //const Producto = () => {
+    function Producto() {
 
         const productosPorPag = 5;
+
+        const [productos, setProductos] = useState([]); // Estado para almacenar los productos
+        /*const [filtroCategoria, setFiltroCategoria] = useState(''); // Estado para el filtro de categoría
+        const [filtroPrecio, setFiltroPrecio] = useState(''); // Estado para el filtro de precio
+        const [busquedaTexto, setBusquedaTexto] = useState(''); // Estado para el campo de búsqueda*/
+        const [busqueda, setBusqueda] = useState('');
+
+
 
         return (
             <div className="producto">
