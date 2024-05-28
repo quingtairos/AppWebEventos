ade14bf2afc216acd6fc1d3179e4916e5b34e05b
maria <mariaquintairosfernandez03@liceolapaz.net>
2024-05-28 21:54:53 +0200

    añadir datos a la pagina de iniciar sesion

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,11 +1,25 @@
 import './Login.css';
 
-const Login = () => {
+/* const Login = () => { */
+    function Login() {
+          // Lógica de inicio de sesión
+
     /* return (
         <div className="login">
             <h1>Login</h1>
         </div>
     ) */
+
+    return (
+        <div className="IniciarSesion">
+          <h2>Iniciar Sesión</h2>
+          {/* Formulario de inicio de sesión */}
+
+          {/* ... */}
+          
+        </div>
+      );
+
 } 
 
 export default Login;
