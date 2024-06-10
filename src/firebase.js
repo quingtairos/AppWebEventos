6d6341fc5b9c1902666544742d5ee1a03800e5ec
maria <mariaquintairosfernandez03@liceolapaz.net>
2024-06-10 21:20:45 +0200

    modificar archivo firebase

--- src/firebase.js
+++ src/firebase.js
@@ -5,6 +5,9 @@ import 'firebase/firestore';*/
 import { initializeApp } from "firebase/app";
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
+
+import 'firebase/firestore';
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
