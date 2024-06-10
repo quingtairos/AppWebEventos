1b49fe806f6620368136cc14b45c6a3c32b2fcbc
maria <mariaquintairosfernandez03@liceolapaz.net>
2024-06-10 21:25:00 +0200

    modificar archivo firebase solucionando error

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,9 @@
 // Import the functions you need from the SDKs you need
-/*import firebase from 'firebase/app';
-import 'firebase/firestore';*/
+import 'firebase/firestore';
 
 import { initializeApp } from "firebase/app";
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
-
-import 'firebase/firestore';
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -24,13 +21,18 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
+//firebase.initializeApp(firebaseConfig);
+
 const auth = getAuth(app);
 const db = getFirestore(app);
 
+//const firestore = firebase.firestore();
+
+//export { firestore };
 /*firebase.initializeApp(firebaseConfig);
 
 export const firestore = firebase.firestore();*/
 
-export { auth, db };
+    export { auth, db };
 
 export default app;
